e03d4f1016589045a8a2174bfaabe1ab8326574a
xiangyufan <xiangyufan@juwang.cn>
2018-01-10 14:37:41 +0800

    debug C2

--- src/Chapter2/main.go
+++ src/Chapter2/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func T1() {
+	// say hello
+	var name string
+
+	fmt.Println("What is your name?")
+	fmt.Scanf("%s", &name)
+
+	fmt.Printf("Hello %s, nice to see you!\n", name)
+}
+
+func T2() {
+	// count str
+	var str string
+
+	fmt.Println("What is the input string?")
+	fmt.Scanf("%s", &str)
+
+	fmt.Printf("%s has %d characters.\n", str, len(str))
+}
+
+func main() {
+	T1()
+	T2()
 }
