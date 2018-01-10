d2ddcad68c4810017e69b848c06359f748471aa8
xiangyufan <xiangyufan@juwang.cn>
2018-01-10 11:00:29 +0800

    Initial commit

---
+++ src/Chapter1/Tips/Tips.go
@@ -0,0 +1,5 @@
+package Tips
+
+func Tips(total float32, tip int) float32 {
+	return float32(total) * float32(tip) / 100.0
+}
---
+++ src/Chapter1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	Tips "Chapter1/Tips"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var total float32    // 总金额可能是小数
+	var tip int
+
+	fmt.Println("请输入总金额：")
+	_, err := fmt.Scanf("%f", &total)
+	if err != nil {
+		fmt.Println("请输入正确的数值类型")
+		os.Exit(0)
+	}
+
+	fmt.Println("请输入小费比例：")
+	fmt.Scanf("%d", &tip)
+
+	tips := Tips.Tips(total, tip)
+	totalTips := float32(total) - tips
+	fmt.Printf("小费金额是：%.2f\n", tips)
+	fmt.Printf("应付金额是：%.2f\n", totalTips)
+}
